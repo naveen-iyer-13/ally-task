db1a2e0ec6a055460d6b82644c03271d371d2b4c
naveen-iyer-13 <naveeniyer13@gmail.com>
2020-04-22 15:44:30 +0530

    Updates

---
+++ precache-manifest.3015a8958f2ab162efbeec9faae226ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0abc002c094a1f08237b0e7024abd43",
+    "url": "/ally-task/index.html"
+  },
+  {
+    "revision": "59322f871bfd188ddd63",
+    "url": "/ally-task/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "c6a09d309baad7b4207a",
+    "url": "/ally-task/static/css/main.bad45173.chunk.css"
+  },
+  {
+    "revision": "59322f871bfd188ddd63",
+    "url": "/ally-task/static/js/2.f8b6a1aa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ally-task/static/js/2.f8b6a1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a09d309baad7b4207a",
+    "url": "/ally-task/static/js/main.8286aa19.chunk.js"
+  },
+  {
+    "revision": "91a43708d4b0fc547f85",
+    "url": "/ally-task/static/js/runtime-main.176e9ae7.js"
+  }
+]);
